ea1a43009656e88fe4e34d9fcc32b8098e2c1b1d
Franco Ortiz <ortizfranco48@yahoo.com>
2024-03-13 19:55:03 -0300

    db connected

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
-import express from "express"
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-const app = express()
+dotenv.config();
 
-app.listen(3000, ()=>{
-    console.log("running on http://localhost:3000")
-})
+mongoose
+  .connect(process.env.DB_URI)
+  .then(() => {
+    console.log("db connected");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+const app = express();
+const port = 8080;
+app.listen(port, () => {
+  console.log(`running on http://localhost:${port}`);
+});
